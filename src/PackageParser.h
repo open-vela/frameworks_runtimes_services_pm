b662b5b4c0bcabe05e8cd13caf51bb6661375b17
zhangheng9 <zhangheng9@xiaomi.com>
2023-07-17 18:46:52 +0800

    add parse class
    parse manifest.json to PackageInfo
    
    Signed-off-by: zhangheng9 <zhangheng9@xiaomi.com>

---
+++ src/PackageParser.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <optional>
+
+#include "PackageUtils.h"
+#include "os/pm/PackageInfo.h"
+
+namespace os {
+namespace pm {
+
+enum ApplicationType {
+    UNKNOWN = -1,
+    NATIVE = 1,
+    QUICKAPP,
+};
+
+class PackageParser {
+public:
+    int parseManifest(const char *path, PackageInfo *info);
+
+private:
+    ApplicationType getApplicationType(const std::string &str);
+    int parseNativeManifest(const rapidjson::Document &document, PackageInfo *info);
+    int parseQuickAppManifest(const rapidjson::Document &document, PackageInfo *info);
+}; // class PackageParser
+
+} // namespace pm
+} // namespace os
