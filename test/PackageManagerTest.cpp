8f1e1596b588bed2bba21f9e3bb4ef63d1015638
zhangheng9 <zhangheng9@xiaomi.com>
2023-07-25 18:30:07 +0800

    add install test case
    Change-Id: Ia94dc136d1cf8399f8ee3c184c2026706565c7b7

--- test/PackageManagerTest.cpp
+++ test/PackageManagerTest.cpp
@@ -14,26 +14,48 @@
  * limitations under the License.
  */
 
+#include <binder/ProcessState.h>
 #include <gtest/gtest.h>
+
+#include <condition_variable>
+#include <mutex>
 
 #include "pm/PackageManager.h"
 
 namespace os {
 namespace pm {
 
+using android::binder::Status;
+
 class PmTest : public testing::Test {
 public:
+    void wake() {
+        std::unique_lock<std::mutex> lock(mMutex);
+        return mCond.notify_one();
+    }
+
+    void wait() {
+        std::unique_lock<std::mutex> lock(mMutex);
+        mCond.wait(lock);
+    }
+
 protected:
     virtual void SetUp() override {
         mExistPackage = "com.vela.demo";
         mNotExistPackage = "com.vela.demo1";
+        mExistRpkPath = "/data/package/com.application.demo.debug.1.0.0.rpk";
+        mNotExistRpkPath = "/data/package/com.vela.demo.rpk";
     }
     virtual void TearDown() override {}
 
 public:
     PackageManager pm;
+    std::condition_variable mCond;
+    std::mutex mMutex;
     std::string mExistPackage;
     std::string mNotExistPackage;
+    std::string mExistRpkPath;
+    std::string mNotExistRpkPath;
 };
 
 TEST_F(PmTest, GetNotExistPackage) {
@@ -47,7 +69,59 @@ TEST_F(PmTest, GetExistPackage) {
     EXPECT_STREQ(mExistPackage.c_str(), info.packageName.c_str());
 }
 
+class InstallListenerTest : public BnInstallObserver {
+public:
+    InstallListenerTest(PmTest &test, int32_t result) : mTest(test), mExpectResult(result) {}
+
+    Status onInstallProcess(const std::string &packageName, int32_t process) override {
+        return Status::ok();
+    }
+
+    Status onInstallResult(const std::string &packageName, int32_t code,
+                           const std::string &msg) override {
+        EXPECT_EQ(code, mExpectResult);
+        mTest.wake();
+        return Status::ok();
+    }
+
+private:
+    PmTest &mTest;
+    int32_t mExpectResult;
+};
+
+TEST_F(PmTest, InstallExistPackage) {
+    InstallParam param;
+    param.path = mExistRpkPath;
+    sp<InstallListenerTest> listener = sp<InstallListenerTest>::make(*this, 0);
+    int ret = pm.installPackage(param, listener);
+    EXPECT_EQ(ret, 0);
+    wait();
+}
+
+TEST_F(PmTest, InstallNotExistPackage) {
+    InstallParam param;
+    param.path = mNotExistRpkPath;
+    sp<InstallListenerTest> listener =
+            sp<InstallListenerTest>::make(*this, android::NAME_NOT_FOUND);
+    int ret = pm.installPackage(param, listener);
+    EXPECT_EQ(ret, 0);
+    wait();
+}
+
+TEST_F(PmTest, InstallRepeatPackage) {
+    InstallParam param;
+    param.path = mExistRpkPath;
+    sp<InstallListenerTest> listener = sp<InstallListenerTest>::make(*this, 0);
+    int ret = pm.installPackage(param, listener);
+    EXPECT_EQ(ret, 0);
+    wait();
+    ret = pm.installPackage(param, listener);
+    EXPECT_EQ(ret, 0);
+    wait();
+}
+
 extern "C" int main(int argc, char **argv) {
+    android::ProcessState::self()->startThreadPool();
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
